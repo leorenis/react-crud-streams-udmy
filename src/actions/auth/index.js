b3a3ff010723c26d620bb9eb7d59b690e9f12cf1
leorenis <leorenis@gmail.com>
2019-05-22 06:59:09 -0300

    Add action startGoogleAuth with async await.

--- src/actions/auth/index.js
+++ src/actions/auth/index.js
@@ -6,19 +6,16 @@ import {
 } from '../types'
 
 export const startGoogleAuth = authChangeCallback => dispatch => {
-  window.gapi.load('client:auth2', () => {
-    window.gapi.client
-      .init({
-        clientId:
-          '1090905734118-eigdmnvhf5k2t6104g03mvpr974cdgfd.apps.googleusercontent.com',
-        scope: 'email'
-      })
-      .then(() => {
-        const auth = window.gapi.auth2.getAuthInstance()
-        dispatch({ type: START_GOOGLE_AUTH, payload: auth })
-        authChangeCallback(auth.isSignedIn.get())
-        auth.isSignedIn.listen(authChangeCallback)
-      })
+  window.gapi.load('client:auth2', async () => {
+    await window.gapi.client.init({
+      clientId:
+        '1090905734118-eigdmnvhf5k2t6104g03mvpr974cdgfd.apps.googleusercontent.com',
+      scope: 'email'
+    })
+    const auth = window.gapi.auth2.getAuthInstance()
+    dispatch({ type: START_GOOGLE_AUTH, payload: auth })
+    authChangeCallback(auth.isSignedIn.get())
+    auth.isSignedIn.listen(authChangeCallback)
   })
 }
 
