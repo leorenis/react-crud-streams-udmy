b6c07ee8841d50f98016164c89a4b41a4d978859
leorenis <leorenis@gmail.com>
2019-05-23 08:12:57 -0300

    Add redux form

--- src/components/Streams/StreamCreate/index.js
+++ src/components/Streams/StreamCreate/index.js
@@ -1,21 +1,66 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import _ from 'lodash'
 
 class StreamCreate extends React.Component {
+  onSubmit(formValues) {
+    console.log(formValues)
+  }
 
-    renderInput () {
-        return <input />
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className={'ui error message'}>
+          <div className={'heander'}>{error}</div>
+        </div>
+      )
     }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    )
+  }
+
   render() {
     return (
-      <form>
-        <Field name={'title'} component={this.renderInput} />
-        <Field name={'description'} component={this.renderInput} />
+      <form
+        className={'ui form error'}
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field
+          name={'title'}
+          component={this.renderInput}
+          label={'Enter Title'}
+        />
+        <Field
+          name={'description'}
+          component={this.renderInput}
+          label={'Enter Description'}
+        />
+        <button className={'ui button primary'}> Submit </button>
       </form>
     )
   }
 }
 
+const validate = formValues => {
+  const errors = {}
+  _.forEach(['title', 'description'], field => {
+    if (!formValues[field]) {
+      errors[field] = `You must enter a ${field}`
+    }
+  })
+  return errors
+}
+
 export default reduxForm({
-  form: 'streamCreateForm'
+  form: 'streamCreateForm',
+  validate
 })(StreamCreate)
