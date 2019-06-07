f4091960f12a07c1ab1c1f5c7c7f4c150b990dd5
leorenis <leorenis@gmail.com>
2019-06-07 07:05:46 -0300

    Add Strem

--- src/components/Streams/StreamShow/index.js
+++ src/components/Streams/StreamShow/index.js
@@ -1,18 +1,44 @@
 import React from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 
 import { fetchStream } from '../../../actions/streams'
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props)
+    this.videoRef = React.createRef()
+  }
+
   componentDidMount() {
     const { fetchStream, match } = this.props
     fetchStream(match.params.id)
+    this.buildPlayer()
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer()
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return
+    }
+
+    const { id } = this.props.match.params
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
   }
 
   render() {
     const { stream } = this.props
     return stream ? (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{stream.title}</h1>
         <h5>{stream.description}</h5>
       </div>
