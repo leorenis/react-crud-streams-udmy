86ba7ed2d4d168994ec364e739efd3cea4404c20
leorenis <leorenis@gmail.com>
2020-08-23 20:02:44 -0300

    Studying.

--- src/components/Streams/StreamDelete/index.js
+++ src/components/Streams/StreamDelete/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-
 import { fetchStream, deleteStream } from '../../../actions/streams'
 import Modal from '../../Modal'
 import history from '../../../history'
--- src/components/Streams/StreamEdit/index.js
+++ src/components/Streams/StreamEdit/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
-
 import { fetchStream, editStream } from '../../../actions/streams'
 import StreamForm from '../StreamForm'
 
